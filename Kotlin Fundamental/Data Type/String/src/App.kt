6a13211767cb380d6056240f83802e97b2596e05
Nafia Rizky Yogayana <nafia.ry@gmail.com>
2021-04-27 10:52:38 +0700

    pelatihan Dicoding Kotlin

---
+++ Kotlin Fundamental/Data Type/String/src/App.kt
@@ -0,0 +1,9 @@
+// main function
+fun main() {
+    val text  = "Kotlin"
+    val firstChar = text[0]
+    val lastChar = text[5]
+
+    println("First character of $text is $firstChar")
+    print("Last character of $text is $lastChar")
+}
---
+++ Membangun dan Menjalankan Program Kotlin/Latihan/Latihan 2/src/Task.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val kotlinAwesome = """
+        Kotlin,
+        is Awesome!
+    """.trimIndent()
+
+    print(kotlinAwesome)
+}
